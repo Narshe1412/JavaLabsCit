ef913ceda777fe85cff198dbcf734346fab094c4
Manuel Colorado <manuel1412@gmail.com>
2017-03-04 20:01:13 +0000

    week 3 exercises

--- P2 W3 Testing MVC/src/controller/CarController.java
+++ P2 W3 Testing MVC/src/controller/CarController.java
@@ -4,13 +4,17 @@ import model.Car;
 public class CarController {
 
 	public static final int MAX_INVENTORY=20;
-	Car[] inventory = new Car[MAX_INVENTORY];
+	private Car[] inventory = new Car[MAX_INVENTORY];
 	private int index = 0;
 
 	
 	public Boolean addCar(String make, String model, int year, Boolean sold){
 		Boolean success = false;
-		if (index >= MAX_INVENTORY) {
+		if (make.length()<=0) {
+			System.out.println("You have not entered a car name") ;
+		} else if (model.length()<=0) {
+			System.out.println("You have not entered a car model");
+		} else if (index >= MAX_INVENTORY) {
 			System.out.println("Too many cars already on the system.");
 		} else {
 			inventory[index] = new Car(make, model, year, sold);
@@ -28,6 +32,10 @@ public class CarController {
 					" Year: " + inventory[i].getYear() + 
 					" Sold? " + (inventory[i].isSold()? "Yes": "No"));
 		}
+	}
+	
+	public Car[] getCollection(){
+		return this.inventory;
 	}
 	
 }
--- P2 W3 Testing MVC/src/test/CarTest.java
+++ P2 W3 Testing MVC/src/test/CarTest.java
@@ -2,6 +2,7 @@ package test;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import controller.CarController;
+import model.Car;
 
 public class CarTest {
 
@@ -9,7 +10,50 @@ public class CarTest {
 	public void testAddCar() {
 		CarController Test1 = new CarController();
 		assertEquals(Test1.addCar("ford", "mondeo", 1900, true), true) ;
-		fail("Not yet implemented");
 	}
 
+	@Test
+	public void testMaxNumberOfCars(){
+		CarController Test2 = new CarController();
+		for (int i=0; i<Test2.MAX_INVENTORY; i++) {
+			assertEquals(Test2.addCar("Ford", "mondeo", 1900, true), true);
+		}
+	}
+	
+	@Test
+	public void testOverMaxNumberOfCars(){
+		CarController Test3 = new CarController();
+		for (int i=0; i<Test3.MAX_INVENTORY; i++) {
+			assertEquals(Test3.addCar("Ford", "mondeo", 1900, true), true);
+		}
+		assertEquals(Test3.addCar("Ford", "mondeo", 1900, true), false);
+	}
+	
+	@Test
+	public void noMakeIncluded(){
+		CarController Test4 = new CarController();
+		assertEquals(Test4.addCar("", "mondeo", 1900, true), false);
+	}
+	
+	@Test
+	public void noModelIncluded(){
+		CarController Test5 = new CarController();
+		assertEquals(Test5.addCar("Ford", "", 1900, true), false);
+	}
+	
+	@Test
+	public void verifyDataHasBeenAdded(){
+		CarController Test6 = new CarController();
+		Test6.addCar("Ford", "Mondeo", 1990, true);
+		
+		Car[] carTest = Test6.getCollection();
+		
+		assertEquals(carTest[0].getMake(), "Ford");
+		assertEquals(carTest[0].getModel(), "Mondeo");
+		assertEquals(carTest[0].getYear(), 1990);
+		assertEquals(carTest[0].isSold(), true);
+		
+	}
+	 
+	
 }
